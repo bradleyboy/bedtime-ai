f421341399519c06a63055d262c080158d44bcab
Brad Daily <brad@bradleyboy.com>
2023-12-17 00:17:14 -0500

    fix screen height on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   content: ['./{pages,components}/**/*.{js,tsx}'],
   theme: {
     extend: {
+      height: {
+        // Use dvh when able, falling back for old browsers.
+        // fixes mobile screen height
+        screen: ['100vh', '100dvh'],
+      },
       fontFamily: {
         sans: ['Andika', ...defaultTheme.fontFamily.sans],
       },
