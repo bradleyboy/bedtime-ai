959817753cf80d95b5ac99ec09efc3479c50bd2e
Brad Daily <brad@bradleyboy.com>
2024-04-06 12:13:40 -0400

    Add GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,26 @@
 import { NOKKIO_CSP_NONCE, Html, Head, Body, DocumentProps } from '@nokkio/doc';
 
+const gtagInit = `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-9K0VDCYFPJ');
+`;
+
 export default function Document({ children }: DocumentProps): JSX.Element {
   return (
     <Html lang="en">
       <Head>
+        <script
+          nonce={NOKKIO_CSP_NONCE}
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9K0VDCYFPJ"
+        ></script>
+        <script
+          nonce={NOKKIO_CSP_NONCE}
+          dangerouslySetInnerHTML={{ __html: gtagInit }}
+        ></script>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
