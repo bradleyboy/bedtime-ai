831581f526af0de5dc06ada2e1b2fd47f4629079
Brad Daily <brad@bradleyboy.com>
2025-02-08 15:47:01 -0500

    try to make daily stories more unique

--- server/schedules/[day]/21/00.ts
+++ server/schedules/[day]/21/00.ts
@@ -1,4 +1,4 @@
-import { User } from '@nokkio/magic';
+import { Story, User } from '@nokkio/magic';
 
 // Using Nokkio's scheduling feature, create a new public story every day
 // at 21:00 UTC.
@@ -9,8 +9,19 @@ export default async function () {
     return;
   }
 
+  const recent = await Story.find({
+    filter: {
+      isDailyStory: true,
+      isPublic: true,
+    },
+    sort: '-createdAt',
+    limit: 5,
+  });
+
+  const history = recent.map((r) => r.text).join('\n');
+
   await user.createStory({
-    prompt: 'tell me a bedtime story',
+    prompt: `write a unique bedtime story. Here are the last 5 stories you created, the new story should not use the same characters, themes, or storylines: ${history}`,
     isPublic: true,
     isDailyStory: true,
   });
